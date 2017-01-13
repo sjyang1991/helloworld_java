bf41e1434594a1500af7feb0868362ddced0e255
양성준 <sjyang1991@naver.com>
2017-01-13 14:20:34 +0900

    main 오타 수정

--- VariableExam/src/com/test/variable_exam/VariableExam.java
+++ VariableExam/src/com/test/variable_exam/VariableExam.java
@@ -1,7 +1,7 @@
 package com.test.variable_exam;
 
 public class VariableExam {
-	public static void mai(String args[]){
+	public static void main(String args[]){
 		int value = 10;
 		int result = value+10;
 		System.out.println(result);
