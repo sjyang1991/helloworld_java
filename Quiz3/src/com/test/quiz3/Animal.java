f760d94cb7bec8580f70ef1885055d64f1562975
양성준 <sjyang1991@naver.com>
2017-01-17 15:03:28 +0900

    alive 추가

--- Quiz3/src/com/test/quiz3/Animal.java
+++ Quiz3/src/com/test/quiz3/Animal.java
@@ -1,44 +1,80 @@
 package com.test.quiz3;
 
 public class Animal{
+	//필드
 	int length;	//몸 길이
 	int weight;//무게
 	int speed;//이동속도
+	int age;//나이
 	int life;//수명
-	int skin;//표면
+	int skin;//표면 (1=털, 2=깃털, 3=딱딱, 4=미끈)
 	int legs;//다리 수
 	int wings;//날개쌍
 	int feed;//먹이(1=동물, 2=식물, 3=잡식)
 	int breathe;//숨쉬는 방법
 	int habitat;//사는 곳
 	int horns;//뿔개수
+	int strength;//체력
+	boolean alive;//생존
 	
+	//생성자
 	public Animal() {
-		length =0; weight=0; speed=0; 
-		life=0; skin=0; legs=0; wings=0; 
-		feed=0; breathe=0; habitat=0; horns=0;
+		length =0; weight=0; speed=0; life=0; skin=0; legs=0; wings=0; feed=0; breathe=0; habitat=0; horns=0; alive=true; /*strength=weight;*/
 	}
 	
+	//메소드
 	boolean eat(Animal feed){
 		if(this.feed ==1 || this.feed ==3){
-			int satiety = feed.weight /10;
-			this.weight += satiety;
-			return true;
+			if (this.alive == true) {
+				int satiety = feed.weight /10;
+				this.weight += satiety;
+				this.strength += satiety * 2;
+				return true;
+			}
+			
 		}
 		return false;
 	}
 	boolean eat(Plant feed){
 		if(this.feed ==2 || this.feed ==3){
-			return true;
+			if (this.alive == true) {
+				return true;
+			}
 		}
 		return false;
 	}
-	
-	
-	
-	
-	
-	
+	boolean attack(Animal enemy){
+		if (this.alive == true && enemy.alive == true) {
+			//방어력
+			int defence = enemy.weight;
+			defence = defence /(enemy.age*100/enemy.life);
+			defence += enemy.speed * (Math.random()*10);
+			if(enemy.skin == 3) defence += defence*0.2;
+			else if(enemy.skin == 4) defence += defence*0.3;
+			System.out.println("방어력:"+defence);		//debug 코드
+			
+			//공격력
+			int attack = this.weight;
+			if(this.feed ==1) attack += attack*0.3;
+			attack += this.speed * (Math.random()*10);
+			attack += this.horns * (Math.random()*5);
+			if(this.wings > 0) attack *= 2;
+			System.out.println("공격력:"+attack);		//debug 코드
+			
+			//데미지
+			int demage = attack - defence;
+			if(demage > 0) {
+				enemy.strength -= demage;
+				if(enemy.strength < 0) enemy.alive = false;
+				return true;
+			}
+			else if(demage < 0)	{
+				this.strength += demage;
+				if(this.strength < 0) this.alive = false;
+			}
+		}
+		return false;
+	}
 	
 	
 	
--- Quiz3/src/com/test/quiz3/AnimalExam.java
+++ Quiz3/src/com/test/quiz3/AnimalExam.java
@@ -11,6 +11,23 @@ public class AnimalExam {
 		Plant carrot = new Plant();
 		System.out.println(tiger.eat(carrot));
 		System.out.println(cow.eat(carrot));
+		///////////////////////////////////
+		Animal dog = new Animal();
+		dog.strength = dog.weight = 27;	dog.skin=1; dog.speed=35; dog.life=15; dog.age=8; dog.feed=3;
+
+		tiger.strength = tiger.weight = 310; tiger.skin=1; tiger.speed=65; tiger.life=26; tiger.age=2;
+		
+		Animal bear = new Animal();
+		bear.strength = bear.weight = 600; bear.skin=1; bear.speed=40; bear.life=20; bear.age=3; bear.feed=3;
+		
+		System.out.println(dog.attack(tiger));
+		System.out.println(dog.strength);
+		
+		System.out.println(tiger.attack(bear));
+		System.out.println(bear.strength);
+		System.out.println(bear.attack(tiger));
+		System.out.println(tiger.strength);
+		
 		
 	}
 		
